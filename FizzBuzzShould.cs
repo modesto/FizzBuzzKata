c8f7c2baf452db5e06f58583704a8866defa2b8b
Modesto San Juan <modestosja@gmail.com>
2016-08-18 00:39:55 +0100

    Green: FizzBuzz should return the same number

---
+++ FizzBuzzShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FizzBuzzKata
+{
+    public class FizzBuzzShould {
+        private readonly FizzBuzz fizzBuzz;
+
+        public FizzBuzzShould() {
+            fizzBuzz = new FizzBuzz();
+        }
+        [Fact]
+        public void return_the_same_number() {
+            fizzBuzz.Calculate(1).Should().Be("1");
+        }
+    }
+
+    public class FizzBuzz {
+        public string Calculate(int value) {
+            return value.ToString();
+        }
+    }
+}
